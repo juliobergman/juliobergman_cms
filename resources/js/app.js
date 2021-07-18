069a281c943f414bc684c83b8cebc5481f989ae8
Julio Bergman <me@juliobergman.com>
2021-07-18 16:02:11 -0500

    Vue js Installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import Vue from "vue";
+
+// Main Pages
+import GuestContainer from "../js/views/guest.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: { GuestContainer },
+});
---
+++ resources/js/views/guest.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data: () => ({
+        message: "Hello Guest"
+    }),
+    methods: {},
+    created() {}
+};
+</script>
+
+<style></style>
