09fe6ed2679522589680d7316f0e85e05cd94b8a
Julio Bergman <me@juliobergman.com>
2021-09-26 12:14:31 -0500

    app

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Julio Bergman'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://www.juliobergman.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
